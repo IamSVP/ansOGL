e2883fbbc5f16823e05c235e43e3ac170d0f26ae
IamSVP <psrihariv@cs.unc.edu>
2016-07-06 15:51:02 -0400

    first shitty commit

---
+++ ans_ogl/ans_ogl.h
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+
+
+// Include opengl wrapper 
+#include <GL/glew.h>
+
+// Include Window management library
+#include <glfw3.h>
+
+// Include glm 
+#include <glm/glm.hpp>
+
+
+
